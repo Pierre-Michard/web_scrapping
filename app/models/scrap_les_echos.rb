9d12fbf1f665a7a0de83346174e215a9bb7a7877
pmichard <pierre.michard@parrot.com>
2016-03-18 19:56:14 +0100

    [DEVMINOR] : limit les echos search to 1 year

--- app/models/scrap_les_echos.rb
+++ app/models/scrap_les_echos.rb
@@ -4,7 +4,20 @@ class ScrapLesEchos
   end
 
   def count_articles
-    page = HTTParty.get("http://recherche.lesechos.fr/?exec=1&texte=#{@company_name}")
+    end_date = Date.today
+    start_date = end_date - 1.year
+    @options = {
+        query:{
+          jourd:start_date.day,
+          moisd:start_date.month,
+          anneed:start_date.year,
+          jourf:end_date.day,
+          moisf:end_date.month,
+          anneef:end_date.year,
+          text: @company_name
+        }
+    }
+    page = HTTParty.get("http://recherche.lesechos.fr/", @options)
     parse_page = Nokogiri::HTML(page)
     parse_page.css('.bloc-tri').css('.resultats-search').css('strong').text.gsub(/[^0-9]/, '').to_i
   end
